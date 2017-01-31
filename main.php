f8ebcaa64f9e9e8fdc2cc38a55e9fbad81b4445f
Andrew Gillingham <andrew@leadsnearby.com>
2017-01-31 10:12:17 -0500

    Bug fixes

--- main.php
+++ main.php
@@ -3,23 +3,24 @@
 Plugin URI: http://leadsnearby.com/
 Description: Creates admin page to enter global schema and adds a meta box to each page add schema page description and select custom schema itemtype for clients with more than one business veritical.
 
-Version: 1.1.3
+Version: 1.1.4
 Author: LeadsNearby
 Author URI: http://leadsnearby.com/
 License: GPLv2 or later
 */
 
+// Definitions
+define('SchemaOptions_MAIN', plugin_dir_path( __FILE__ ));
+
 // Enqueue Styles
 function lnb_schema_styles() { 
-	wp_register_style('schema-styles', plugins_url('/schema-options/css/schema-styles.css'));
+	wp_register_style('schema-styles', plugins_url( 'css/schema-styles.css', __FILE__ ) );
 	wp_enqueue_style('schema-styles');	
 }
 add_action('wp_enqueue_scripts', 'lnb_schema_styles');
 add_action('admin_head', 'lnb_schema_styles');
 
 // Load Additional Files
-define('SchemaOptions_MAIN', plugin_dir_path( __FILE__ ));
-
 require_once(SchemaOptions_MAIN . '/shortcodes.php');
 require_once(SchemaOptions_MAIN . '/meta-boxes.php');
 
