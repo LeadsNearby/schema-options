04698527775616f12fa337763dbdb9ed7dca45be
Andrew Gillingham <andrew@leadsnearby.com>
2019-08-02 11:44:49 -0400

    Add options to support lat long functionality

--- lib/admin/admin-init.php
+++ lib/admin/admin-init.php
@@ -37,14 +37,31 @@ class schema_admin_page {
 
         if (count($_POST) > 0) {
             $options = array(
+                'schema_mapik',
                 'schema_itemtype',
                 'schema_tel',
                 'schema_email',
                 'schema_url',
                 'schema_address_street',
                 'schema_pricerange',
-                'schema_logo',
             );
+            $validMapik = true;
+            if(!empty($_POST['schema_mapik'])) {
+                $response = wp_remote_get("https://maps.googleapis.com/maps/api/geocode/json?address={$_POST['schema_address_street']}&key={$_POST['schema_mapik']}");
+                $body = json_decode(wp_remote_retrieve_body($response), true);
+                if( $body['status'] == 'REQUEST_DENIED') {
+                    $validMapik = false;
+                    echo '<div class="notice notice-error"><p>Invalid Google Maps Api Key</p></div>';
+                    add_action('admin_notices', function() {
+                    });
+                    $_POST['schema_mapik'] = '';
+                }
+                if (isset($body['results'][0]) && $validMapik) {
+                    $_POST['schema_address_street'] = $body['results'][0]['formatted_address'];
+                    update_option('lnb_schema_address_lat', $body['results'][0]['geometry']['location']['lat']);
+                    update_option('lnb_schema_address_lng', $body['results'][0]['geometry']['location']['lng']);
+                }
+            }
 
             foreach ($options as $opt) {
                 $old_value = get_option('lnb_' . $opt);
@@ -62,6 +79,14 @@ class schema_admin_page {
 			<h1>Schema for <?php echo get_bloginfo('name'); ?></h1>
 			<fieldset>
 				<table class="form-table" style="max-width: 750px">
+					<tr>
+						<td class="description">
+							<label for="schema_mapik">Google Maps Api Key</label>
+						</td>
+						<td class="form-field">
+                            <input name="schema_mapik" id="schema_mapik" type="text" value="<?php echo get_option('lnb_schema_mapik')?>" />
+						</td>
+					</tr>
 					<tr>
 						<td class="description">
 							<label for="schema_itemtype">Select Type of Business</label>
@@ -102,6 +127,18 @@ class schema_admin_page {
 										<input name="schema_address_street" type="text" id="schema_address_street" value="<?php echo get_option('lnb_schema_address_street'); ?>"/>
 									</td>
 								</tr>
+                                <tr>
+                                    <td>Geo Coordinates:
+                                        <?php
+                                            $lat = get_option('lnb_schema_address_lat', '');
+                                            $lng = get_option('lnb_schema_address_lng', '');
+                                        ?>
+                                        <?php if(!empty($lat) && !empty($lng)): ?>
+                                        <input type="text" id="lat" readonly value="<?php echo $lat; ?>">
+                                        <input type="text" id="long" readonly value="<?php echo $lng; ?>">
+                                        <?php endif; ?>
+                                    </td>
+                                </tr>
 							</table>
 						</td>
 					</tr>
